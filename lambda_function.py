f0968645416ea0ba7ae40af6949bd7ef95f2f6bc
Fuyuan Wang <60631776+wfy1997@users.noreply.github.com>
2021-01-21 15:41:52 -0500

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -85,7 +85,7 @@ def lambda_handler(event, context):
         #file_name=sqlresult[1]
         file_name=messageJson['org_file_name']
         validation_date=messageJson['validation_date']
-        file_submitted_by=messageJson['file_submitted_by'][0:len(messageJson['file_submitted_by'])]
+        file_submitted_by=messageJson['file_submitted_by'][1:len(messageJson['file_submitted_by'])-1]
         file_status="processed"
         org_file_id=messageJson['org_file_id']
         
